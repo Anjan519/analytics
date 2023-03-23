054bbe7afc0dddeb4c5bbb53a44baa0f1c95c9ef
Anjan519 <127855745+Anjan519@users.noreply.github.com>
2023-03-23 10:54:29 +0530

    Add files via upload

---
+++ 2 dimensional.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+intmain()
+{
+	int a[5][5];
+	int i,j,r,c;
+	
+	printf("enter row and col ");
+	scanf("%d%d",&r,&c);
+	
+	for(i=0;i<r;i++)
+	 for(j=0;j<c;j++)
+	    scanf("%d,"&a[i],[j]);
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		 if(i==j
+		 )
+		 printf("%d,",a[i][j]);
+		 //printf("\n");
+		 }     
+}
